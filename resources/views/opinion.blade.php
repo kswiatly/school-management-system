c88fb668bd1ac5228d82b1025d43ecc3ac6f209e
Daniel Wieczorek <wieczorek.daniel@outlook.com>
2020-04-20 18:03:14 +0200

    opinions-and-elearning-materials

---
+++ resources/views/opinion.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Your opinion</div>
+                <div class="card-body">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSes5lLR2KKRf_E3l2ENdlhZOaSPJE0eZfrMjJuf4WdqraV1BQ/viewform?embedded=true" width="680" height="940" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
